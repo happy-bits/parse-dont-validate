a3080da71df7f82935099bcb2c2873835c178777
Oscar Olsson <happybits@gmail.com>
2023-04-19 23:32:54 +0200

    A solution with pattern matching

---
+++ ParseDontValidate.UI/Good1b.cs
@@ -0,0 +1,52 @@
+﻿namespace ParseDontValidate.UI;
+
+internal class Good1b
+{
+
+    public static void Run()
+    {
+        NonEmptyList list = GetListFromUser();
+
+        string first = GetFirstElement(list);
+
+        Console.WriteLine($"The first element in uppercase: {first.ToUpper()}");
+
+    }
+
+
+    private static NonEmptyList GetListFromUser()
+    {
+        while (true)
+        {
+
+            Console.Write("Enter a comma separated list: ");
+            string[] array = Console.ReadLine()!.Trim().Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            if (array.Length > 0)
+                return new NonEmptyList(array);
+
+        }
+    }
+
+    private static string GetFirstElement(NonEmptyList list)
+    {
+        return list.Value[0];
+    }
+
+    // Same as Good1 but with "pattern matching"
+
+    class NonEmptyList
+    {
+        private readonly List<string> _items;
+
+        public NonEmptyList(IEnumerable<string> items)
+        { 
+            _items = items.ToList() switch {
+                var x when x.Count > 0 => x,
+                _ => throw new ArgumentException("The list cannot be empty", nameof(items))
+            };
+        }
+
+        public List<string> Value => _items;
+    }
+}
--- ParseDontValidate.UI/Program.cs
+++ ParseDontValidate.UI/Program.cs
@@ -5,6 +5,9 @@ ParseDontValidate.UI.Bad1.Run();
 Console.WriteLine("\nGood1\n");
 ParseDontValidate.UI.Good1.Run();
 
+Console.WriteLine("\nGood1b\n");
+ParseDontValidate.UI.Good1b.Run();
+
 Console.WriteLine("\nGood2\n");
 ParseDontValidate.UI.Good2.Run();
 
